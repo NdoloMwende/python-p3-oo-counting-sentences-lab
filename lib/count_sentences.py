e3f1059375ebb624c7af3671e950e92cf652c38b
Ndolo Mercy <mercymwendend444@gmail.com>
2025-09-28 14:49:01 +0300

    Completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
+import re
 class MyString:
-  pass
+  def __init__(self,value = ""):
+    self.value = value
+   
+  @property
+  def value (self):
+    return self._value
+  
+  @value.setter
+  def value(self,value):
+    if isinstance(value,str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+  
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+    new_sentences = re.split(r'[?.!]',self.value)
+    new_sentences= [s.strip() for s in new_sentences if s.strip()]
+    return len(new_sentences)
+  
+mystring= MyString("He!My name is Ethan.What is yours?You look hungry.Want some food?")
+
+mystring.is_sentence()
+mystring.is_question()
+mystring.is_exclamation()
+mystring.count_sentences()
+  
+  
